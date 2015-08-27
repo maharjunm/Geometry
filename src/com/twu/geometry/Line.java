5b3796ccb5f2bbcb7db1ef805b7d2d680ed23312
maharjun <maharjun@thoughtworks.com>
2015-08-27 23:22:26 +0530

    Refoctoring of the code

--- src/com/twu/geometry/Line.java
+++ src/com/twu/geometry/Line.java
@@ -3,32 +3,40 @@ package com.twu.geometry;
 import static java.lang.Math.*;
 
 public class Line {
-    private double x1,y1,x2,y2;
+    private double x1, y1, x2, y2;
+
     public Line(double x1, double y1, double x2, double y2) {
-        this.x1=x1;
-        this.y1=y1;
-        this.x2=x2;
-        this.y2=y2;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 
     public double length() {
 
-        return sqrt(pow(x2 - x1,2)+pow(y2 - y1, 2));
+        return sqrt(squareDifference(x2, x1) + squareDifference(y2, y1));
+    }
+
+    private double squareDifference(double value1, double value2) {
+        return pow(value1 - value2, 2);
     }
 
 
     @Override
     public boolean equals(Object other) {
-        if(other==null)
+        if (other == null || !(other instanceof Line))
             return false;
-        if(!(other instanceof Line))
-            return false;
-        Line otherLine=(Line)other;
-        if(x1==otherLine.x1 && y1==otherLine.y1 && x2==otherLine.x2 && y2==otherLine.y2)
-            return true;
-        if(x1==otherLine.x2 && y1==otherLine.y2 && x2==otherLine.x1 && y2==otherLine.y1)
-            return true;
-        return false;
+        Line otherLine = (Line) other;
+        return compareLineWithCoordinatePairsInterchangibly(otherLine);
+    }
+
+    private boolean compareLineWithCoordinatePairsInterchangibly(Line otherLine) {
+        return compareLineWithCoordinatePairs(otherLine.x1, otherLine.y1, otherLine.x2, otherLine.y2) ||
+                compareLineWithCoordinatePairs(otherLine.x2, otherLine.y2, otherLine.x1, otherLine.y1);
+    }
+
+    private boolean compareLineWithCoordinatePairs(double otherX1, double otherY1, double otherX2, double otherY2) {
+        return x1 == otherX1 && y1 == otherY1 && x2 == otherX2 && y2 == otherY2;
     }
 
 }
--- test/com/twu/geometry/LineTest.java
+++ test/com/twu/geometry/LineTest.java
@@ -7,159 +7,159 @@ public class LineTest {
 
     @Test
     public void shouldCalculateLengthAsZeroForStartingPointAndEndingPointAsSame() {
-        Line line=new Line(0.0,0.0,0.0,0.0);
-        double actual=line.length();
+        Line line = new Line(0.0, 0.0, 0.0, 0.0);
+        double actual = line.length();
 
-        Assert.assertEquals(0.0,actual,0.2d);
+        Assert.assertEquals(0.0, actual, 0.2d);
     }
 
     @Test
     public void shouldCalculateLengthForALineWhichLiesOnXAxis() {
-        Line line=new Line(1,0.0,2,0.0);
-        double actual=line.length();
+        Line line = new Line(1, 0.0, 2, 0.0);
+        double actual = line.length();
 
-        Assert.assertEquals(1.0,actual,0.2d);
+        Assert.assertEquals(1.0, actual, 0.2d);
     }
 
     @Test
     public void shouldCalculateLengthForALineWhichLiesOnYAxis() {
-        Line line=new Line(0,1,0,2);
-        double actual=line.length();
+        Line line = new Line(0, 1, 0, 2);
+        double actual = line.length();
 
-        Assert.assertEquals(1.0,actual,0.2d);
+        Assert.assertEquals(1.0, actual, 0.2d);
     }
 
     @Test
     public void shouldCalculateLengthForALine() {
-        Line line=new Line(1,1,2,2);
-        double actual=line.length();
+        Line line = new Line(1, 1, 2, 2);
+        double actual = line.length();
 
-        Assert.assertEquals(Math.sqrt(2),actual,0.2d);
+        Assert.assertEquals(Math.sqrt(2), actual, 0.2d);
     }
 
     @Test
     public void shouldBeEqualWhenLineComparesItself() {
-        Line line=new Line(1,1,2,2);
+        Line line = new Line(1, 1, 2, 2);
 
-        Assert.assertEquals(line,line);
+        Assert.assertEquals(line, line);
     }
 
     @Test
     public void shouldNotBeEqualWhenLineComparesWithANull() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=null;
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = null;
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenLineComparesWithOtherObject() {
-        Line line1=new Line(1,1,2,2);
+        Line line1 = new Line(1, 1, 2, 2);
 
-        Assert.assertNotEquals(line1,2);
+        Assert.assertNotEquals(line1, 2);
     }
 
     @Test
     public void shouldNotBeEqualWhenLineWithOtherLineWhichHasX1ValuesAsDifferentAndOtherAsSame() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(0,1,2,2);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(0, 1, 2, 2);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenLineWithOtherLineWhichHasY1ValuesAsDifferentAndOtherAsSame() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(1,2,2,2);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(1, 2, 2, 2);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenLineWithOtherLineWhichHasX2ValuesAsDifferentAndOtherAsSame() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(1,1,3,2);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(1, 1, 3, 2);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenLineWithOtherLineWhichHasY2ValuesAsDifferentAndOtherAsSame() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(1,1,2,3);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(1, 1, 2, 3);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenX1OfLine1AndX2OfLine2AreDifferentButOtherValuesAreEqual() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(2,2,2,1);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(2, 2, 2, 1);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenY1OfLineAndY2OfLine2AreDifferentButOtherValuesAreEqual() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(2,2,1,2);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(2, 2, 1, 2);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenX2OfLine1DifferFromLine2OfX1andOtherValuesAreSame() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(1,2,1,1);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(1, 2, 1, 1);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenY2OfLine1DifferFromLine2OfY1andOtherValuesAreSame() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(2,1,1,1);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(2, 1, 1, 1);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldBeEqualWhenLinesHaveSameStartingAndEndingPoints() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(1,1,2,2);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(1, 1, 2, 2);
 
-        Assert.assertEquals(line1,line2);
+        Assert.assertEquals(line1, line2);
     }
 
     @Test
     public void shouldBeEqualWhenLinesHaveStaringPointsAndEndingPointsWereInterchanges() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(2,2,1,1);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(2, 2, 1, 1);
 
-        Assert.assertEquals(line1,line2);
+        Assert.assertEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenY1OfLine1AndY2OfLineAreNotSameAndRestOfThemAreSame() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(2,2,1,2);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(2, 2, 1, 2);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenX2OfLine1AndX1OfLineAreNotSameAndRestOfThemAreSame() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(3,2,1,1);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(3, 2, 1, 1);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 
     @Test
     public void shouldNotBeEqualWhenY2OfLine1AndY1OfLineAreNotSameAndRestOfThemAreSame() {
-        Line line1=new Line(1,1,2,2);
-        Line line2=new Line(2,4,1,1);
+        Line line1 = new Line(1, 1, 2, 2);
+        Line line2 = new Line(2, 4, 1, 1);
 
-        Assert.assertNotEquals(line1,line2);
+        Assert.assertNotEquals(line1, line2);
     }
 }
