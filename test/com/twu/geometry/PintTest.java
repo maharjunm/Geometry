db78b9d31331fc7355d00b978f1d3e4e0bdb4ebf
maharjun <maharjun@thoughtworks.com>
2015-08-28 00:22:11 +0530

    Point should not be equal to null point

--- test/com/twu/geometry/PintTest.java
+++ test/com/twu/geometry/PintTest.java
@@ -6,8 +6,15 @@ import org.junit.Test;
 public class PintTest {
 
     @Test
-    public void shouldBeTrueWhenCompareWithItself(){
+    public void shouldBeEqualWhenCompareWithItself(){
         Point point=new Point(1.0,2.0);
         Assert.assertEquals(point,point);
+    }
+
+    @Test
+    public void shouldNotBeEqualWhenCompareWithNullPoint(){
+        Point point1=new Point(1.0,2.0);
+        Point point2=null;
+        Assert.assertNotEquals(point1,point2);
     }
 }
