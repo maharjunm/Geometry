ade8ebf91034dbe180b756d8d07f4d0d9a0b35e5
maharjun <maharjun@thoughtworks.com>
2015-08-28 08:23:02 +0530

    Pint should not be equal with different Y values

--- test/com/twu/geometry/PintTest.java
+++ test/com/twu/geometry/PintTest.java
@@ -3,31 +3,40 @@ package com.twu.geometry;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class PintTest {
 
     @Test
     public void shouldBeEqualWhenCompareWithItself(){
         Point point=new Point(1.0,2.0);
-        Assert.assertEquals(point,point);
+        assertEquals(point, point);
     }
 
     @Test
     public void shouldNotBeEqualWhenCompareWithNullPoint(){
         Point point1=new Point(1.0,2.0);
         Point point2=null;
-        Assert.assertNotEquals(point1,point2);
+        assertNotEquals(point1, point2);
     }
 
     @Test
     public void shouldNotBeEqualWhenCompareWithOtherInstance(){
         Point point1=new Point(1.0,2.0);
-        Assert.assertNotEquals(point1,2);
+        assertNotEquals(point1, 2);
     }
 
     @Test
     public void shouldNotBeEqualWhenXisNotEqual(){
         Point point1=new Point(1.0,2.0);
         Point point2=new Point(2.0,2.0);
-        Assert.assertNotEquals(point1,point2);
+        assertNotEquals(point1, point2);
+    }
+
+    @Test
+    public void shouldNotBeEqualWhenYisNotEqual(){
+        Point point1=new Point(1.0,2.0);
+        Point point2=new Point(1.0,3.0);
+        assertNotEquals(point1, point2);
     }
 }
