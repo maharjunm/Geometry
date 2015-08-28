b3ed324a0b32505cb67b621e50d5b4e4769fc15d
maharjun <maharjun@thoughtworks.com>
2015-08-28 08:37:09 +0530

    Modified Code for other Objects

--- src/com/twu/geometry/Point.java
+++ src/com/twu/geometry/Point.java
@@ -2,21 +2,23 @@ package com.twu.geometry;
 
 public class Point {
 
-    private double x,y;
+    private double x, y;
 
     public Point(double x, double y) {
-        this.x=x;
-        this.y=y;
+        this.x = x;
+        this.y = y;
     }
 
     @Override
-    public boolean equals(Object other){
-        if(other==null)
+    public boolean equals(Object other) {
+        if (other == null)
             return false;
-        if(other==this)
+        if (!(other instanceof Point))
+            return false;
+        if (other == this)
             return true;
-        Point otherPoint=(Point)other;
-        if(x==otherPoint.x && y==otherPoint.y)
+        Point otherPoint = (Point) other;
+        if (x == otherPoint.x && y == otherPoint.y)
             return true;
         return false;
     }
--- test/com/twu/geometry/PintTest.java
+++ test/com/twu/geometry/PintTest.java
@@ -8,50 +8,50 @@ import static org.junit.Assert.*;
 public class PintTest {
 
     @Test
-    public void shouldBeEqualWhenCompareWithItself(){
-        Point point=new Point(1.0,2.0);
+    public void shouldBeEqualWhenCompareWithItself() {
+        Point point = new Point(1.0, 2.0);
         assertEquals(point, point);
     }
 
     @Test
-    public void shouldNotBeEqualWhenCompareWithNullPointModified(){
-        Point point1=new Point(1.0,2.0);
-        Point point2=null;
+    public void shouldNotBeEqualWhenCompareWithNullPointModified() {
+        Point point1 = new Point(1.0, 2.0);
+        Point point2 = null;
         assertNotEquals(point1, point2);
     }
 
     @Test
-    public void shouldNotBeEqualWhenCompareWithOtherInstance(){
-        Point point1=new Point(1.0,2.0);
+    public void shouldNotBeEqualWhenCompareWithOtherInstance() {
+        Point point1 = new Point(1.0, 2.0);
         assertNotEquals(point1, 2);
     }
 
     @Test
-    public void shouldNotBeEqualWhenXisNotEqual(){
-        Point point1=new Point(1.0,2.0);
-        Point point2=new Point(2.0,2.0);
+    public void shouldNotBeEqualWhenXisNotEqualModified() {
+        Point point1 = new Point(1.0, 2.0);
+        Point point2 = new Point(2.0, 2.0);
         assertNotEquals(point1, point2);
     }
 
     @Test
-    public void shouldNotBeEqualWhenYisNotEqual(){
-        Point point1=new Point(1.0,2.0);
-        Point point2=new Point(1.0,3.0);
+    public void shouldNotBeEqualWhenYisNotEqual() {
+        Point point1 = new Point(1.0, 2.0);
+        Point point2 = new Point(1.0, 3.0);
         assertNotEquals(point1, point2);
     }
 
     @Test
-    public void shouldBeEqualWhenBothHasSamePoints(){
-        Point point1=new Point(1.0,2.0);
-        Point point2=new Point(1.0,2.0);
+    public void shouldBeEqualWhenBothHasSamePoints() {
+        Point point1 = new Point(1.0, 2.0);
+        Point point2 = new Point(1.0, 2.0);
         assertEquals(point1, point2);
     }
 
 
     @Test
-    public void modifiedCodeForPointsWhenBothHavingDifferentYValues(){
-        Point point1=new Point(1.0,2.0);
-        Point point2=new Point(1.0,3.0);
+    public void modifiedCodeForPointsWhenBothHavingDifferentYValues() {
+        Point point1 = new Point(1.0, 2.0);
+        Point point2 = new Point(1.0, 3.0);
         assertNotEquals(point1, point2);
     }
 }
